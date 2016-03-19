6af8892d143549769f3f4f84721e06d118cc0125
Sohifudin <sohifudin@gmail.com>
2016-03-19 16:25:33 +0700

    P1

---
+++ TwistInTaleNamberSystems.java
@@ -0,0 +1,11 @@
+class TwistInTaleNumberSystems {
+	public static void main(String... args) {
+		int baseDecimal = 267;
+		int octVal = 0413;
+		int hexVal = 0x10B;
+		int binVal = 0b100001011;
+		System.out.println(baseDecimal + octVal);
+		System.out.println(hexVal + binVal);
+	}
+}
+		
